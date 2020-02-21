7580c293efa7525136e7d75dc8bf44875de16a17
Mitzi Estrada <m_estrada17@live.com>
2020-02-21 08:08:22 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -17,7 +17,9 @@ def age_appropriate_birthday(birthday_kids)
     if age <= 12
       "Happy Birthday #{kids_name}! You are now #{age} years old!"
     else
-      "You are too old for this"
+      "You are too old for this."
+    end
+  end
   
 end
 
