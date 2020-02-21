24517e14b63e3a3d4817b9618f8d85ca01b90570
Mitzi Estrada <m_estrada17@live.com>
2020-02-21 08:08:07 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,8 +14,10 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    if age < 12
-      "Happy Birthday #{kids_name}! You are now #{age} old!"
+    if age <= 12
+      "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    else
+      "You are too old for this"
   
 end
 
