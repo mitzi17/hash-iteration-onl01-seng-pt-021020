c784b0563c76a09f5984e33efc94b68d270a0d9f
Mitzi Estrada <m_estrada17@live.com>
2020-02-21 08:05:24 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,7 +12,11 @@ def happy_birthday(birthday_kids)
   end
 end
 
-def age_appropriate_birthday
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    if age < 12
+      "Happy Birthday, "
+  
 end
 
 
